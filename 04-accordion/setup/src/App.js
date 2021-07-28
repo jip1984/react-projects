039598ee0541093867b4da6a66f42a6b71620b69
jon <jonprosser84@gmail.com>
2021-07-28 15:20:30 +0100

    questions finished

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
+import questions from './data';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
 
-  let title = data.map((d) => {
-    return d.title;
-  })
-  return <>
-    <div>
-      <SingleQuestion title={title} />
+  const [question, setQuestion] = useState(data)
+
+  return <main>
+    <div className="container">
+      <h3>Questions And Answers</h3>
+      <section className="info">
+        {
+          questions.map((question) => {
+            return <SingleQuestion key={question.id}{...question} />
+          })
+        }
+      </section>
     </div>
-  </>;
+  </main>;
 }
 
 export default App;
