fd21f98133cac3f859e71284756bafad52bd47f1
jon <jonprosser84@gmail.com>
2021-07-28 13:33:42 +0100

    accordion

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+
+  let title = data.map((d) => {
+    return d.title;
+  })
+  return <>
+    <div>
+      <SingleQuestion title={title} />
+    </div>
+  </>;
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title }) => {
+  return <main>
+    <div className="container">
+      <h3>{title}</h3>
+    </div>
+  </main>;
 };
 
 export default Question;
